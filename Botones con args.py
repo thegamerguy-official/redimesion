bd77ac974127feb9d4d8e481530eb4719ddc9f6f
thegamerguy-official <229004806+thegamerguy-official@users.noreply.github.com>
2026-04-25 18:52:23 +0200

    Explicacion añadida Botones con argumentos

--- Botones con args.py
+++ Botones con args.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from tkinter import ttk
-
+# Esta lógica se basa en usar funciones lambda y al llamar una funcion podemos
+# Usar los parentesis para usar argumentos en funciones que loo requieran
+# Este caso es una funcion que tiene como arg texto a printear.
 window = tk.Tk()
 
 def boton_func(text):
